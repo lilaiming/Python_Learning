c88caf1282e4d1554118d3b6c45149f4f22f9619
lilaiming <441314423@qq.com>
2022-01-09 12:31:26 +0800

    Initial commit

---
+++ print.py
@@ -0,0 +1,8 @@
+# -*- coding=utf-8 -*-
+# 本脚由lilaiming编写，用于学习使用！
+# Email:essid@qq.com
+
+print('welcome to qytang!')
+print('welcome to lenovo!')
+print('welcome to python!')
+print('github commit 2')
